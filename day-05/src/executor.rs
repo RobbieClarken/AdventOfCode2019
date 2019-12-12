d15f1f8d1467a9ffedb53e4f2e8af9284e4f4c2f
RobbieClarken <robbie.clarken@gmail.com>
2019-12-12 19:06:58 +1030

    Day 5: Handle immediate mode params

--- day-05/src/executor.rs
+++ day-05/src/executor.rs
@@ -11,6 +11,24 @@ enum Opcode {
     Output = 4,
 }
 
+#[derive(FromPrimitive)]
+enum Mode {
+    Position = 0,
+    Immediate = 1,
+}
+
+struct ModeGenerator {
+    val: i32,
+}
+
+impl ModeGenerator {
+    fn next(&mut self) -> Mode {
+        let mode = FromPrimitive::from_i32(self.val % 10).unwrap();
+        self.val /= 10;
+        mode
+    }
+}
+
 pub struct Executor<'a> {
     program: &'a mut Vec<i32>,
     input: VecDeque<i32>,
@@ -28,11 +46,11 @@ impl<'a> Executor<'a> {
         };
         loop {
             match executor.read_opcode() {
-                Opcode::Halt => break,
-                Opcode::Input => executor.op_input(),
-                Opcode::Output => executor.op_output(),
-                Opcode::Add => executor.op_add(),
-                Opcode::Multiply => executor.op_multiply(),
+                (Opcode::Halt, _) => break,
+                (Opcode::Input, _) => executor.op_input(),
+                (Opcode::Output, mg) => executor.op_output(mg),
+                (Opcode::Add, mg) => executor.op_add(mg),
+                (Opcode::Multiply, mg) => executor.op_multiply(mg),
             }
         }
         executor.output
@@ -44,14 +62,21 @@ impl<'a> Executor<'a> {
         val
     }
 
-    fn read_opcode(&mut self) -> Opcode {
-        let opcode = self.read();
-        FromPrimitive::from_i32(opcode).expect(&format!("unexpected opcode: {}", opcode))
+    fn read_opcode(&mut self) -> (Opcode, ModeGenerator) {
+        let val = self.read();
+        let opcode = val % 100;
+        let mode_gen = ModeGenerator { val: val / 100 };
+        let opcode =
+            FromPrimitive::from_i32(opcode).expect(&format!("unexpected opcode: {}", opcode));
+        (opcode, mode_gen)
     }
 
-    fn read_param(&mut self) -> i32 {
-        let addr = self.read() as usize;
-        self.program[addr]
+    fn read_param(&mut self, mode_gen: &mut ModeGenerator) -> i32 {
+        let mut v = self.read();
+        if let Mode::Position = mode_gen.next() {
+            v = self.program[v as usize];
+        }
+        v
     }
 
     fn op_input(&mut self) {
@@ -59,25 +84,25 @@ impl<'a> Executor<'a> {
         self.program[out_addr] = self.input.pop_front().expect("insufficient input values");
     }
 
-    fn op_output(&mut self) {
-        let v = self.read_param();
+    fn op_output(&mut self, mut mode_gen: ModeGenerator) {
+        let v = self.read_param(&mut mode_gen);
         self.output.push(v)
     }
 
-    fn op_add(&mut self) {
-        self.perform_binary_op(|x, y| x + y);
+    fn op_add(&mut self, mut mode_gen: ModeGenerator) {
+        self.perform_binary_op(|x, y| x + y, &mut mode_gen);
     }
 
-    fn op_multiply(&mut self) {
-        self.perform_binary_op(|x, y| x * y);
+    fn op_multiply(&mut self, mut mode_gen: ModeGenerator) {
+        self.perform_binary_op(|x, y| x * y, &mut mode_gen);
     }
 
-    fn perform_binary_op<F>(&mut self, op: F)
+    fn perform_binary_op<F>(&mut self, op: F, mode_gen: &mut ModeGenerator)
     where
         F: FnOnce(i32, i32) -> i32,
     {
-        let param1 = self.read_param();
-        let param2 = self.read_param();
+        let param1 = self.read_param(mode_gen);
+        let param2 = self.read_param(mode_gen);
         let output_address = self.read() as usize;
         self.program[output_address] = op(param1, param2);
     }
@@ -119,6 +144,37 @@ mod executor_tests {
         assert_eq!(out, vec![4, 99]);
     }
 
+    #[test]
+    fn handles_immediate_mode_for_first_param() {
+        let mut arr: Vec<i32> = vec![
+            101, // 0: add (param 1 is immediate mode)
+            20,  // 1: immediate mode value = 20
+            7,   // 2: addr 7 value = 30
+            0,   // 3: write to addr 0
+            4,   // 4: output
+            0,   // 5: value in addr 0
+            99,  // 6: halt
+            30,  // 7
+        ];
+        let out = Executor::run(&mut arr, vec![]);
+        assert_eq!(out, vec![50]);
+    }
+
+    #[test]
+    fn handles_immediate_mode_for_second_param() {
+        let mut arr: Vec<i32> = vec![
+            1001, // 0: add (param 2 is immediate mode)
+            7,    // 1: addr 7 value = 20
+            30,   // 2: immediate mode value value = 30
+            0,    // 3: write to addr 0
+            4,    // 4: output
+            0,    // 5: value in addr 0
+            99,   // 6: halt
+            20,   // 7
+        ];
+        let out = Executor::run(&mut arr, vec![]);
+        assert_eq!(out, vec![50]);
+    }
 
     #[test]
     fn handles_more_complicated_cases() {
