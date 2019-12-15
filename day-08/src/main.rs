51d5e24504de64bff0b9013dda4922ffedf27c0b
RobbieClarken <robbie.clarken@gmail.com>
2019-12-15 11:42:22 +1030

    Day 8: Solve puzzle 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -1,13 +1,28 @@
 use std::fs::File;
 use std::io::Read;
 
+const BLACK: u32 = 0;
+const WHITE: u32 = 1;
+const TRANSPARENT: u32 = 2;
+
+const WIDTH: usize = 25;
+const HEIGHT: usize = 6;
+
 fn main() {
     challenge_1();
+    challenge_2();
 }
 
 fn challenge_1() {
-    let solution = solve_challenge_1(read_data(), 25, 6);
+    let solution = solve_challenge_1(read_data(), WIDTH, HEIGHT);
     println!("Challenge 1: Solution = {}", solution);
+}
+
+fn challenge_2() {
+    let layers = build_layers(read_data(), WIDTH, HEIGHT);
+    let layer = flatten(&layers);
+    println!("Challenge 2:");
+    print!("{}", to_string(&layer, WIDTH, HEIGHT));
 }
 
 fn solve_challenge_1(data: Vec<u32>, width: usize, height: usize) -> u32 {
@@ -64,6 +79,34 @@ fn count_digit(digit: u32, values: &[u32]) -> u32 {
     count
 }
 
+fn flatten(layers: &[Vec<u32>]) -> Vec<u32> {
+    let mut out = layers[0].clone();
+    for layer in layers.iter().skip(1) {
+        for (v, w) in out.iter_mut().zip(layer) {
+            if *v == TRANSPARENT {
+                *v = *w;
+            }
+        }
+    }
+    out
+}
+
+fn to_string(image: &[u32], width: usize, height: usize) -> String {
+    let mut image = image.iter();
+    let mut out: Vec<&str> = Vec::new();
+    for _ in 0..height {
+        for _ in 0..width {
+            out.push(match *image.next().unwrap() {
+                BLACK => " ",
+                WHITE => "█",
+                _ => unreachable!(),
+            });
+        }
+        out.push("\n");
+    }
+    out.join("")
+}
+
 #[cfg(test)]
 mod test_day_8 {
     use super::*;
@@ -92,5 +135,38 @@ mod test_day_8 {
             2, 2, 2, 0, //
         ];
         assert_eq!(solve_challenge_1(data, 4, 2), 12);
+    }
+
+    #[test]
+    fn flattens() {
+        let data = build_layers(
+            vec![
+                0, 2, // layer 1
+                2, 2, //
+                1, 1, // layer 2
+                2, 2, //
+                2, 2, // layer 3
+                1, 2, //
+                0, 0, // layer 4
+                0, 0,
+            ],
+            2,
+            2,
+        );
+        let expected = vec![
+            0, 1, //
+            1, 0, //
+        ];
+        assert_eq!(flatten(&data), expected);
+    }
+
+    #[test]
+    fn converts_image_to_string() {
+        let image = vec![
+            0, 0, 1, //
+            1, 1, 1, //
+        ];
+        let expected = "  █\n███\n";
+        assert_eq!(to_string(&image, 3, 2), expected);
     }
 }
