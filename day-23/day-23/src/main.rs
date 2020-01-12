12186352415cf2659a1489065181e33b87d74efa
RobbieClarken <robbie.clarken@gmail.com>
2020-01-12 23:07:29 +0100

    Day 23: Solve puzzle 2

--- day-23/day-23/src/main.rs
+++ day-23/day-23/src/main.rs
@@ -3,6 +3,7 @@ use std::collections::VecDeque;
 
 fn main() {
     challenge_1();
+    challenge_2();
 }
 
 fn challenge_1() {
@@ -27,6 +28,46 @@ fn challenge_1() {
     };
     println!(
         "Challenge 1: The first Y value sent to address 255 = {}",
+        answer
+    );
+}
+
+fn challenge_2() {
+    let mut computers: Vec<_> = (0..50).map(|_| Computer::load_from_file("input")).collect();
+    let mut queue: VecDeque<(usize, Vec<i64>)> = VecDeque::new();
+    let mut nat_packet: Option<Vec<i64>> = None;
+    let mut last_y: Option<i64> = None;
+    for addr in 0..50 {
+        queue.push_back((addr, vec![addr as i64, -1]));
+    }
+    let answer = loop {
+        while !queue.is_empty() {
+            let (addr, packets) = queue.pop_front().expect("ran out of packets on queue");
+            let computer = &mut computers[addr];
+            let (mut output, _) = computer.run(packets);
+            while !output.is_empty() {
+                let target_addr = output.remove(0) as usize;
+                let x = output.remove(0);
+                let y = output.remove(0);
+                if target_addr == 255 {
+                    nat_packet = Some(vec![x, y]);
+                } else {
+                    queue.push_back((target_addr, vec![x, y]));
+                }
+            }
+        }
+        let x = nat_packet.clone().unwrap()[0];
+        let y = nat_packet.clone().unwrap()[1];
+        if let Some(last_y) = last_y {
+            if last_y == y {
+                break y;
+            }
+        }
+        queue.push_back((0, vec![x, y]));
+        last_y = Some(y);
+    };
+    println!(
+        "Challenge 2: The first Y value delivered by the NAT twice = {}",
         answer
     );
 }
